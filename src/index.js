965d47f71cbefbd6113d2754e0873134eb5b16fd
Justin <aerosphia.business@gmail.com>
2021-11-29 20:43:00 -0800

    Removed unnecessary intents

--- src/index.js
+++ src/index.js
@@ -7,11 +7,9 @@ const client = new Client({
     intents: [
         Intents.FLAGS.GUILDS,
         Intents.FLAGS.GUILD_MESSAGES,
-        Intents.FLAGS.GUILDS,
         Intents.FLAGS.DIRECT_MESSAGES,
         Intents.FLAGS.GUILD_MEMBERS,
         Intents.FLAGS.GUILD_PRESENCES,
-        Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
     ],
 
     partials: ["CHANNEL"],
