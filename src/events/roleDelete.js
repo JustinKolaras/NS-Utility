ffa7d12b2ce5e1d1553ec55799775d9a6663935f
Justin <justinkolaras@outlook.com>
2021-12-14 17:59:16 -0800

    Bug fix

--- src/events/roleDelete.js
+++ src/events/roleDelete.js
@@ -13,8 +13,8 @@ module.exports = {
         if (deletedRoleNames.length >= 3) {
             const alertMessage = `@everyone, **Excessive Role Deletion Alert:** \`3 w/i 120s\`\n\`\`\`\n${deletedRoleNames.join("\n")}\n\`\`\``;
 
-            util.getGuild(client, "900218984287313920")
-                .then((guild) => util.getChannel(guild, "906596799630942279"))
+            util.getGuild(client, "761468835600924733")
+                .then((guild) => util.getChannel(guild, "810717109427503174"))
                 .then((channel) => channel.send(alertMessage))
                 .catch(console.error);
 
