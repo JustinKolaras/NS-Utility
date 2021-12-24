2fd874ae4de8e04bc8c08bfa24a5005402284f13
Justin <justinkolaras@outlook.com>
2021-12-23 19:30:38 -0800

    Bug fixes

--- src/commands/isvip.js
+++ src/commands/isvip.js
@@ -28,6 +28,10 @@ class Command {
         let playerId;
         let usingDiscord = false;
 
+        if (!playerName || args.length > 1) {
+            return void Msg.reply("**Syntax Error:** `;isvip <username | @user | userId>`");
+        }
+
         // Discord Mention Support
         const attributes = await util.getUserAttributes(Msg.guild, args[0]);
         if (attributes.success) {
@@ -38,10 +42,6 @@ class Command {
             } else {
                 return void Msg.reply(`Could not get Roblox account via Discord syntax. Please provide a Roblox username.`);
             }
-        }
-
-        if (!playerName || args.length > 1) {
-            return void Msg.reply("**Syntax Error:** `;isvip <username | @user | userId>`");
         }
 
         if (!usingDiscord) {
--- src/commands/rep.js
+++ src/commands/rep.js
@@ -20,11 +20,19 @@ class Command {
         amount = currentStat?.reputationNum || 0;
 
         if (amount < 20) {
-            response = `:innocent: :innocent: Hello! The mighty kingdom of NS Reputation :crown: told me you have **${amount}** reputation points in your bank today. That's not good.. :grimacing: :sleepy: :worried:`;
+            response = `:innocent: :innocent: Hello! The mighty kingdom of NS Reputation :crown: told me you have **${util.sep(
+                amount
+            )}** reputation points in your bank today. That's not good.. :grimacing: :sleepy: :worried:`;
         } else if (amount < 100) {
-            response = `:innocent: :innocent: Hello! The mighty kingdom of NS Reputation :crown: told me you have **${amount}** reputation points in your bank today. Seems pretty average. :grin: :pray:`;
+            response = `:innocent: :innocent: Hello! The mighty kingdom of NS Reputation :crown: told me you have **${util.sep(
+                amount
+            )}** reputation points in your bank today. Seems pretty average. :grin: :pray:`;
         } else if (amount >= 100) {
-            response = `:innocent: :innocent: Hello! The mighty kingdom of NS Reputation :crown: told me you have **${amount}** reputation points in your bank today. :money_with_wings: :money_with_wings: Gosh, go ahead and share some! :gem: :gem: :money_mouth:`;
+            response = `:innocent: :innocent: Hello! The mighty kingdom of NS Reputation :crown: told me you have **${util.sep(
+                amount
+            )}** reputation points in your bank today. :money_with_wings: :money_with_wings: Gosh, go ahead and share some! :gem: :gem: :money_mouth:`;
+        } else {
+            return void Msg.reply("There was an error.");
         }
 
         return void Msg.reply(response);
