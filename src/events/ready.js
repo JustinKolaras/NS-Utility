c28ea10db90d4a40bfca418aca9e3ad5e388d61a
Justin <justinkolaras@outlook.com>
2022-01-09 18:50:13 -0800

    chalk.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,7 +1,9 @@
+const chalk = require("chalk");
+
 module.exports = {
     name: "ready",
     execute() {
         discordClient.user.setActivity(`;help | ${config.version}`, { type: "LISTENING" });
-        console.log("Updated");
+        console.log(chalk.green("Updated"));
     },
 };
