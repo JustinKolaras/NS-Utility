fcffc595adc8a2bd386247769d3e860dab1c20c7
Justin <78659866+Aerosphia@users.noreply.github.com>
2022-01-13 00:08:01 -0800

    calling destroy now leaves the guild

--- src/commands/destroy.js
+++ src/commands/destroy.js
@@ -1,3 +1,5 @@
+const Util = require("../modules/Util");
+
 class Command {
     constructor(options) {
         for (const k in options) {
@@ -7,6 +9,10 @@ class Command {
 
     fn = async (Msg) => {
         try {
+            Msg.guild.leave().catch((err) => {
+                console.error(err);
+                Util.dmUser([config.ownerId], `**Guild Leave Error On \`destroy\`**\n\`\`\`\n${err}\n\`\`\``);
+            });
             discordClient.destroy();
         } catch (err) {
             Msg.reply(`*Error:*\n\`\`\`\n${err}\n\`\`\``);
