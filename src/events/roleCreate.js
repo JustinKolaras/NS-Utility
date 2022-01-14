70878ed628c41f44494109751f502c23403ee5e9
Justin <78659866+Aerosphia@users.noreply.github.com>
2022-01-14 14:16:43 -0800

    roleCreate listener

---
+++ src/events/roleCreate.js
@@ -0,0 +1,18 @@
+const Util = require("../modules/Util");
+
+module.exports = {
+    name: "roleCreate",
+    async execute(newRole) {
+        const hasAdmin = (role) => {
+            return role.permissions.has("ADMINISTRATOR");
+        };
+
+        const prefix = `@everyone, `;
+        const messageToSend = `**${newRole.name}** (${newRole.id}) has been given the \`ADMINISTRATOR\` permission.\nThis is just a notice. If you know this is intentional, please ignore this message.`;
+
+        if (hasAdmin(newRole)) {
+            Util.getChannel(newRole.guild, "810717109427503174").send(prefix + messageToSend);
+            Util.dmUsersIn(newRole.guild, "788877981874389014", `An important server action may need your attention.\n\n${messageToSend}`).catch(() => {});
+        }
+    },
+};
