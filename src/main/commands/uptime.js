c7202e8c9ad53e5d7464c9f8291794f90cff2a8c
Justin <78659866+Aerosphia@users.noreply.github.com>
2022-01-15 14:33:10 -0800

    uptime cmd

---
+++ src/main/commands/uptime.js
@@ -0,0 +1,36 @@
+const { MessageEmbed } = require("discord.js");
+
+const Util = require("../externals/Util");
+
+class Command {
+    constructor(options) {
+        for (const k in options) {
+            this[k] = options[k];
+        }
+    }
+
+    fn = async (Msg) => {
+        const timeParameters = Util.getTimeParameters(discordClient.uptime);
+
+        let messageEmbed;
+        try {
+            messageEmbed = new MessageEmbed()
+                .setColor("#2f3136")
+                .setTitle(`${timeParameters.days}d, ${timeParameters.hours}h, ${timeParameters.minutes}m, ${timeParameters.seconds}s`);
+        } catch (err) {
+            console.error(err);
+            return Msg.channel.send(`**${timeParameters.days}d, ${timeParameters.hours}h, ${timeParameters.minutes}m, ${timeParameters.seconds}s**`);
+        }
+
+        return Msg.channel.send({ embeds: [messageEmbed] });
+    };
+}
+
+module.exports = {
+    class: new Command({
+        Name: "uptime",
+        Description: "Displays bot uptime.",
+        Usage: `;uptime`,
+        Permission: 0,
+    }),
+};
