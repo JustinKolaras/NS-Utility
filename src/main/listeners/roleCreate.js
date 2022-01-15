cffc8a583943062f5984623ff0719d881c59e9cd
Justin <78659866+Aerosphia@users.noreply.github.com>
2022-01-15 05:38:47 -0800

    better error handling

--- src/main/listeners/roleCreate.js
+++ src/main/listeners/roleCreate.js
@@ -11,8 +11,8 @@ module.exports = {
         const messageToSend = `**${newRole.name}** (${newRole.id}) has been given the \`ADMINISTRATOR\` permission.\nThis is just a notice. If you know this is intentional, please ignore this message.`;
 
         if (hasAdmin(newRole)) {
-            Util.getChannel(newRole.guild, "810717109427503174").send(prefix + messageToSend);
             Util.dmUsersIn(newRole.guild, "788877981874389014", `An important server action may need your attention.\n\n${messageToSend}`).catch(() => {});
+            Util.getChannel(newRole.guild, "810717109427503174")?.send(prefix + messageToSend);
         }
     },
 };
