6b30c65ef4846a8f4433de131e636810b0d5e61a
Justin <78659866+Aerosphia@users.noreply.github.com>
2022-02-01 17:51:38 -0800

    Bump noblox.js Latest

--- src/main/listeners/onJoinRequestHandle.js
+++ src/main/listeners/onJoinRequestHandle.js
@@ -5,10 +5,12 @@ module.exports = async (data) => {
     const groupBans = database.collection("groupBans");
 
     const requesterId = data.requester.userId;
+    console.log("Working with", requesterId);
 
     const isGroupBanned = await groupBans.findOne({ id: requesterId });
 
     if (!isGroupBanned) {
+        console.log("Accepted");
         noblox.handleJoinRequest(config.group, requesterId, true).catch((err) => {
             console.error(err);
             Util.dmUser([config.ownerId], `I could not accept a join request from ID \`${requesterId}\`\n\`\`\`\n${err}\n\`\`\``);
