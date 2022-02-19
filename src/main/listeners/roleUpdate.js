810e052d1ad2214e1d6bffb67fad3ffb042ce239
Justin <78659866+Aerosphia@users.noreply.github.com>
2022-02-19 14:48:07 -0800

    Ignore managed roles for alerts

--- src/main/listeners/roleUpdate.js
+++ src/main/listeners/roleUpdate.js
@@ -11,7 +11,7 @@ module.exports = {
         const prefix = `@everyone, `;
         const messageToSend = `**${newRole.name}** (${newRole.id}) has been given the \`ADMINISTRATOR\` permission.\nThis is just a notice. If you know this is intentional, please ignore this message.`;
 
-        if (!hasAdmin(oldRole) && hasAdmin(newRole)) {
+        if (!hasAdmin(oldRole) && hasAdmin(newRole) && !newRole.managed) {
             Util.dmUsersIn(newRole.guild, "788877981874389014", `An important server action may need your attention.\n\n${messageToSend}`).catch(() => {});
             Util.getChannel(newRole.guild, "810717109427503174")?.send(prefix + messageToSend);
         }
