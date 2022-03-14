b11dbbea089c304ac5c1a4be44befc95c099f2cc
Justin <78659866+Aerosphia@users.noreply.github.com>
2022-03-14 13:40:46 -0700

    Don't leave on guildCreate

--- src/main/listeners/guildCreate.js
+++ src/main/listeners/guildCreate.js
@@ -6,10 +6,12 @@ module.exports = {
     execute(guild) {
         let messageToSend = `<@&907752877798330489>, I was added to a guild.\n**Name:** ${guild.name}\n**Id:** ${guild.id}`;
 
+        /*
         guild.leave().catch((err) => {
             console.error(err);
             messageToSend = messageToSend + `\n\nI couldn't leave the guild.\n\`\`\`\n${err}\n\`\`\``;
         });
+        */
 
         Util.sendInChannel("900218984287313920", "907828976083435541", messageToSend);
     },
